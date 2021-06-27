48ce9498e269fb52c143e64e28123744721c0ee0
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-06-28 00:09:29 +0700

    ngay 1

---
+++ DemoLinQ/DemoLinQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Data;
+            int[] Num = { 5, 10, 8, 3, 6, 12};
+
+            // Query
+            IEnumerable<int> SoNum = from a in Num
+                                     where a%2 == 0
+                                     orderby a
+                                     select a;
+            //IEnumerable<int> numQuery2 = Num.Where(num => num % 2 == 0).OrderBy(n => n);
+            var numQuery2 = Num.Where(num => num % 2 == 0).OrderBy(n => n);
+            // Execute
+            foreach (var item in SoNum)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            foreach (var item in numQuery2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
